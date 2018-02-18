a657e9b29386193cece9ebbe3ec217f28737dac1
michaelelgayar@gmail.com <root@thewall.nightswatch>
2018-02-18 11:00:21 -0500

    ssh tesT

--- projects/twitterbot/quote.py
+++ projects/twitterbot/quote.py
@@ -13,7 +13,7 @@ quote_of_the_day = '"'+quoteparser1+'"'+" \n -"+quoteparser2
 
 print quote_of_the_day
 
-
+###
 #logic for manipulating quote ***VERIFIED THIS WORKS***
 #quote = open("/root/scripts/projects/twitterbot/twitter-code/quote.txt","w")
 #quote.write("Hello World")
