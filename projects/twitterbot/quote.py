993c089a99f7edd1709a39eb003fc7ce1155dd8b
root <root@thewall.nightswatch>
2018-02-18 10:31:10 -0500

    Add new quote scraper

---
+++ projects/twitterbot/quote.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env/python
+
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://www.brainyquote.com/quote_of_the_day")
+quotehtml = BeautifulSoup(page.content, 'html.parser')
+
+quoteparser1 = quotehtml.find("a", attrs={"class":"b-qt qt_122774 oncl_q"}).text
+quoteparser2 = quotehtml.find("a", attrs={"class":"bq-aut qa_122774 oncl_a"}).text
+
+quote_of_the_day = '"'+quoteparser1+'"'+" \n -"+quoteparser2
+
+print quote_of_the_day
+
+
+#logic for manipulating quote ***VERIFIED THIS WORKS***
+#quote = open("/root/scripts/projects/twitterbot/twitter-code/quote.txt","w")
+#quote.write("Hello World")
+#quote.close
+
