6a38e88f855ea8bb24a288879f5e0a81561e499c
michaelelgayar@gmail.com <root@thewall.nightswatch>
2018-02-18 11:13:49 -0500

    test

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 rsync_procs=`ps aux | grep melgayar | wc - l`
 $rsync_procs
 
-#if ${rsync_procs} -gt 5; then
+##if ${rsync_procs} -gt 5; then
 #     echo "CRITICAL - Over 1,000 rsync processes running"
 #     exit 2
 #else
